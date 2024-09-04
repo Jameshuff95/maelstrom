5caec5e33495f52e4bebe9765dcf89d4e792268d
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-09-04 05:04:18 -0400

    inital creation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Start from './Components/Start/Start';
+
+function App() {
+  return (
+    <div className="body">
+      <Start />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Start/Start.jsx
@@ -0,0 +1,14 @@
+import './Start.css';
+
+function Start() {
+  return (
+    <div className="start_container">
+      <h1 className="title">Maelstrom</h1>
+      <div className="button_container">
+        <button className="start">Start Game</button>
+      </div>
+    </div>
+  );
+}
+
+export default Start;
